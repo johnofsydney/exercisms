f8c016df3f5951771e25ae8de0c2987d5e043a39
John Coote <john_coote@rocketmail.com>
2018-02-27 19:51:44 +1100

    raindrops

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,19 @@
+function Raindrops() {
+
+
+  this.convert = function(num) {
+    result = ""
+
+    if (num % 3 === 0) { result = result + "Pling"}
+    if (num % 5 === 0) { result = result + "Plang"}
+    if (num % 7 === 0) { result = result + "Plong"}
+
+    if (( num % 3 !== 0 ) && ( num % 5 !== 0) && (num %7 !== 0)) {
+      result = num.toString()
+    }
+
+    return result
+  }
+}
+
+module.exports = Raindrops;
