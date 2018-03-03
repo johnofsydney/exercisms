2e9c39ec2e65be227925aea3f292c0e3a92089b8
John Coote <john_coote@rocketmail.com>
2018-03-04 10:41:02 +1100

    rain

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -2,18 +2,24 @@ function Raindrops() {
 
 
   this.convert = function(num) {
-    result = ""
+    result = "";
 
-    if (num % 3 === 0) { result = result + "Pling"}
-    if (num % 5 === 0) { result = result + "Plang"}
-    if (num % 7 === 0) { result = result + "Plong"}
-
-    if (( num % 3 !== 0 ) && ( num % 5 !== 0) && (num %7 !== 0)) {
-      result = num.toString()
+    if (num % 3 === 0) {
+      result = result + "Pling";
+    }
+    if (num % 5 === 0) {
+      result = result + "Plang";
+    }
+    if (num % 7 === 0) {
+      result = result + "Plong";
     }
 
-    return result
-  }
+    if (( num % 3 !== 0 ) && ( num % 5 !== 0) && (num %7 !== 0)) {
+      result = num.toString();
+    }
+
+    return result;
+  };
 }
 
 module.exports = Raindrops;
