2132798a8d18af0a0cff6a0ee418ebe4c6932301
John Coote <john_coote@rocketmail.com>
2018-11-17 21:50:44 +1100

    more-matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,17 +1,39 @@
 class Matrix
 
+  attr_reader :rows, :columns
+
   def initialize input
-    @input = input
-  end
-
-  def rows
-    @input.scan(/.+/).map do
-      |r| r.split(" ").map { |n| n.to_i }
+    @rows = input.each_line.map do
+      |r| r.split.map(&:to_i)
     end
-  end
-
-  def columns
-    rows.transpose
+    @columns = rows.transpose
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+# class Matrix
+#
+#   def initialize input
+#     @input = input
+#   end
+#
+#   def rows
+#     @input.scan(/.+/).map do
+#       |r| r.split(" ").map { |n| n.to_i }
+#     end
+#   end
+#
+#   def columns
+#     rows.transpose
+#   end
+#
+# end
