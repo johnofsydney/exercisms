d2d929dd186bb9ee4e19bddc55a5a5aaa5b26d6c
John Coote <john_coote@rocketmail.com>
2018-12-23 12:11:56 +1100

    Finished Ruby SpaceAge

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,9 +1,38 @@
+require 'pry'
+# Definitiion of class SpaceAge for Exercism
 class SpaceAge
+  EARTH_YEAR_IN_SECONDS = 31_557_600
+  PLANETS = {
+    Earth: 1,
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132
+  }.freeze
 
-  def initialize seconds
+  def initialize(seconds)
     @seconds = seconds
   end
 
-  
+  PLANETS.each do |planet, _year|
+    define_method("on_#{planet.downcase}") do
+      @seconds.to_f / EARTH_YEAR_IN_SECONDS / PLANETS[planet]
+    end
+  end
 
+  def method_missing(name, *args, &block)
+    raise "'#{name}' is not a method of the class #{self.class}"
+  end
+
+  def respond_to_missing?(*)
+    p 'not sure what to do here but'
+    p "rubocop recommends a 'respond_to_missing?' method"
+  end
 end
+
+a = SpaceAge.new(1_000_000_000)
+
+binding.pry
