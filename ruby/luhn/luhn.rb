b9329f2735620fc18521ceb939de58df5bd79c3a
John Coote <john_coote@rocketmail.com>
2019-06-20 20:01:18 +1000

    tidy luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -2,38 +2,48 @@ require 'pry'
 
 # Luhn class for Exercism
 class Luhn
-  def self.strip_spaces(str)
-    str.gsub(/\s/, '')
-  end
-
-  def self.contains_non_digits(number)
-    return false if number.match(/\D/).nil?
-
-    true
-  end
-
-  def self.double_every_second(n, i)
-    n = n.to_i
-    return n if i.even?
-
-    n *= 2
-    return n if n < 10
-
-    n - 9
-  end
-
   def self.valid? (number)
-    number = strip_spaces number
-    return false if number.length < 2
-    return false if contains_non_digits number
+    @number = strip_spaces number
+    return false if @number.length < 2
+    return false if contains_non_digits
 
-    total =  number
-              .chars
-              .reverse
-              .each_with_index
-              .map { |n, i| double_every_second n, i }
-              .sum
+    (checksum % 10).zero?
+  end
 
-    (total % 10).zero?
+  class << self
+    private
+
+    def checksum
+      @number
+        .chars
+        .reverse
+        .each_slice(2)
+        .map { |arr| double_every_second arr }
+        .flatten
+        .sum
+    end
+
+    def strip_spaces(str)
+      str.gsub(/\s/, '')
+    end
+
+    def contains_non_digits
+      !@number.match(/\D/).nil?
+    end
+
+    def double_every_second(arr)
+      first = arr[0].to_i
+      second = arr[1].to_i
+
+      [first, (double_second second)]
+    end
+
+    def double_second(num)
+      return num * 2 if num * 2 < 10
+
+      (num * 2) - 9
+    end
   end
 end
+
+# binding.pry
