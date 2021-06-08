d67c8b1b6ec9e9a012ab9709783aa092352375e2
John Coote <john_coote@rocketmail.com>
2021-06-09 09:23:31 +1000

    first pass at JS Allergies

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,27 +1,54 @@
 export class Allergies {
   constructor(num) {
     this.num = num
+    this.allergyTable = calculatePatientAllergies(this.num)
   }
 
   allergicTo(food) {
-    return this.num
+    return checker(food, this.allergyTable)
+  }
+
+  list() {
+    return(
+      Object.keys(this.allergyTable)
+    );
   }
 }
 
 const calculatePatientAllergies = (num) => {
   let result = {};
-  let  start = 128
+  let individualScores = Object
+                          .keys(ALLERGY_CONST)
+                          .map(n => { return Number(n) })
+                          .sort((a, b) => b - a)
 
+  while (num > 0) {
+    let numberToCheck = individualScores.shift()
 
+    if (Math.floor(num / numberToCheck) == 1) {
+      let word = ALLERGY_CONST[numberToCheck.toString()]
+      result[word] = true;
+
+      num = num - numberToCheck
+    }
+  }
+
+  return(result)
 }
 
-ALLERGY_CONST = {
-  eggs: 1,
-  peanuts: 2,
-  shellfish: 4,
-  strawberries: 8,
-  tomatoes: 16,
-  chocolate: 32,
-  pollen: 64,
-  cats: 128,
+const ALLERGY_CONST = {
+  1: 'eggs',
+  2: 'peanuts',
+  4: 'shellfish',
+  8: 'strawberries',
+  16: 'tomatoes',
+  32: 'chocolate',
+  64: 'pollen',
+  128: 'cats',
+}
+
+
+const checker = (food, allergyTable) => {
+
+  return (allergyTable[food] == undefined) ? false : allergyTable[food]
 }
