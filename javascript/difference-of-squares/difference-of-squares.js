cc0a6dcdc9fd912cd3bc660c41465a758f70ebcc
John Coote <john_coote@rocketmail.com>
2021-06-09 15:06:30 +1000

    downloaded and finished sum of squares

---
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -0,0 +1,33 @@
+export class Squares {
+  constructor(num) {
+    this.numberArray = makeArray(num)
+  }
+
+  get sumOfSquares() {
+    return (
+      this.numberArray
+        .map((n) => {return(Math.pow(n,2))})
+        .reduce((sum, n) => {return sum + n}, 0)
+    );
+  }
+
+  get squareOfSum() {
+    return (
+      Math.pow(
+        (this.numberArray.reduce((sum, n) => { return sum + n}, 0)), 2
+      )
+    );
+
+  }
+
+  get difference() {
+    return(this.squareOfSum - this.sumOfSquares)
+  }
+}
+
+const makeArray = (num) => {
+  let arr = [...Array(num + 1).keys()] // [0, 1, 2, ... num]
+  arr.shift(); // remove first entry, zero.
+
+  return(arr)
+}
