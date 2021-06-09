ea2837068973ad8712cbfbad0d235a05e12f1922
John Coote <john_coote@rocketmail.com>
2021-06-09 20:38:32 +1000

    done JS luhn

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -1,8 +1,40 @@
-//
-// This is only a SKELETON file for the 'Luhn' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const valid = (string) => {
+  string = removeSpaces(string)
+  if (string.length < 2) { return false }
+  if (invalidCharacters(string)) { return false }
 
-export const valid = () => {
-  throw new Error('Remove this statement and implement this function');
+  const number = getTheNumber(string)
+
+  return( number % 10 == 0)
 };
+
+const removeSpaces = (string) => {
+  return (string.replace(/\s/g, ''));
+}
+
+const invalidCharacters = (string) => {
+  return (string.match(/\D/) ? true : false);
+}
+
+const doTheMath = (num) => {
+  num = num * 2;
+  if ( num  > 9) { num = num - 9 };
+  return(num)
+}
+
+const getTheNumber = (string) => {
+  return(
+    string
+      .split('')
+      .reverse()
+      .map((num) => { return +num })
+      .map((num, index) => {
+        if (index % 2 == 0) {
+          return num
+        } else {
+          return (doTheMath(num))
+        }
+      })
+      .reduce((sum, num) => { return sum + num }, 0)
+  );
+}
