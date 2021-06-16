b74caca0f287eb493bdc24441ec67c1a73d68366
John Coote <john_coote@rocketmail.com>
2021-06-17 07:57:56 +1000

    improved Ruby armstrong numbers

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -2,12 +2,16 @@ class ArmstrongNumbers
   def self.include?(number)
     size = number.size
 
-    number == number.to_s.chars.map{ |d| d.to_i ** size}.sum
+    number == number.chars.map{ |d| d ** size}.sum
   end
 end
 
 class Integer
   def size
     self.to_s.size
+  end
+
+  def chars
+    self.to_s.chars.map{ |d| d.to_i }
   end
 end
