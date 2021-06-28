727d1b5ddec0766ec61c9faf8603fb47c3da6224
John Coote <john_coote@rocketmail.com>
2021-06-29 08:58:56 +1000

    second iteration of secret handshake

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -1,29 +1,36 @@
+require 'pry'
 class SecretHandshake
+
+
   def initialize(number)
     @number = number
   end
 
   def commands
     return [] unless @number.is_a?(Integer)
-    
     commands = []
 
     if @number > 2 ** 4
       reverse_it = true
-      @number = @number - 2 ** 4
+      @number -= 2 ** 4
     end
 
-    [3,2,1,0].each do |index|
-      if @number >= 2 ** index
-        commands.push(COMMAND_LIST[index])
-        @number = @number - 2 ** index
+    COMMAND_LIST
+      .each_with_index
+      .map{ |command, index| {command: command, power: index} }
+      .reverse
+      .each do |h|
+        if @number >= 2 ** h[:power]    # if the number is large enough
+          commands.unshift h[:command]  # add it to the array
+          @number -= 2 ** h[:power]     # and reduce the number accordingly
+        end
       end
 
-    end
-
     commands.reverse! if reverse_it
-    commands.reverse
+    commands
   end
+
+  private
 
   COMMAND_LIST = ['wink', 'double blink', 'close your eyes', 'jump']
 end
